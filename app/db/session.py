dd3409824e8ca1e348b6fcee89c56a53d3d66d40
cnshzs <cnzhangsheng@outlook.com>
2026-05-14 14:45:41 +0800

    更新代码 2026-05-14 14:45

--- app/db/session.py
+++ app/db/session.py
@@ -7,7 +7,7 @@ from app.core.config import settings
 
 engine = create_async_engine(
     settings.database_url,
-    echo=settings.debug,
+    echo=False,
     pool_pre_ping=True,
     connect_args={"charset": "utf8mb4"},
 )
